3c9f11a9d378e7805a7d0965cf32677fa22d2ab9
GimmeSpork <The.horse.butch@gmail.com>
2026-06-06 23:32:44 +0000

    WorkInProgress

--- main.cpp
+++ main.cpp
@@ -1,36 +1,18 @@
-// ------------- FILE HEADER -------------
-// Author ✅: 
-// Assignment ✅:
-// Date ✅:
-// Citations: 
-
-// ------------- CODE -------------
+/******************************************************************************
+# Author:           Lucy P
+# Lab:              Assignment 9
+# Date:             June 6, 2026
+# Description:      prompt user for number of positive digits, and then the
+#                   digits they want before calculating the max and minimum 
+#                   number, in the position it appears. 
+# Output:           min, max, minPosition, maxPosition as ints.
+# Sources:          zybooks, assignment 9 resources, zoom recordings.
+#******************************************************************************/
 #include <iostream>
 
 using namespace std;
 
-
-
-// Have fun with messages
-// Credit to copilot for content of GOODBYE_MESSAGE
-// Format by me.
-// PROMPT: shakespearian goodbye message parting is such sweet sorrow
-const string GOOBYE_MESSAGE = 
-  "\n\n"
-  "\tParting is such sweet sorrow;\n"
-  "\tyet let it be A softened ache\n"
-  "\tthat keeps our memory bright.\n"
-  "\tGo with sunshine in thy step and courage free,\n"
-  "\tTill Fortune weaves our paths together once more.\n"
-  "\tFarewell, dear heart—tread well,\n" 
-  "\tand keep me in thy sight\n";
-
-
-// Function prototypes 
-// Have fun and be creative with welcome and goodbye messages!
-// Or, keep it simple and just print 
-// "Welcome to my Miles Tracker program!" and 
-// "Keep riding!" in these functions.  
+// Function prototypes   
 void welcome();
 void goodbye();
 
@@ -56,114 +38,105 @@ void getInput(int &goal);
 int calcTotal();
 
 int main() {
+  int goal = 0;
   welcome();
-  cout << "TODO: Implement main function" << endl;
+  
+  getInput(goal);
+  if (goal <= 0) {
+    cout << "\nno miles were tracked this week." << endl;
+  }
+
+  calcTotal();
+
   goodbye();
   return 0;
 }
 
 // Function implementations (if any)
 void getInput(int &goal) {
-  cout << "TODO: Implement getInput function" << endl;
+  cout << "\nHow many miles do you want to ride this week?: ";
+  cin >> goal;
 }
 
 int calcTotal() {
-  cout << "TODO: Implement calcTotal function" << endl;
+  int totalMiles = 0;
+  int userInput = 0;
+
+  for(int i = 0; i <= 7; i++){
+    switch(i) {
+      case 0:
+        cout << "\nHow many miles did you ride on Monday?: ";
+        cin >> userInput;
+          if(userInput < 0) {
+            cout << "Miles must be 0 or greater!" << endl;
+            cout << "How many miles did you ride on Monday?: ";
+            cin >> userInput;
+          }
+        totalMiles += userInput;
+      case 1:
+        cout << "How many miles did you ride on Tuesday?: ";
+        cin >> userInput;
+          if(userInput < 0) {
+            cout << "Miles must be 0 or greater!" << endl;
+            cout << "How many miles did you ride on Tuesday?: ";
+            cin >> userInput;
+          }
+        totalMiles += userInput;
+      case 2:
+        cout << "How many miles did you ride on Wednesday?: ";
+        cin >> userInput;
+          if(userInput < 0) {
+            cout << "Miles must be 0 or greater!" << endl;
+            cout << "How many miles did you ride on Wednesday?: ";
+            cin >> userInput;
+          }
+        totalMiles += userInput;
+      case 3:
+        cout << "How many miles did you ride on Thursday?: ";
+        cin >> userInput;
+          if(userInput < 0) {
+            cout << "Miles must be 0 or greater!" << endl;
+            cout << "How many miles did you ride on Thursday?: ";
+            cin >> userInput;
+          }
+        totalMiles += userInput;
+      case 4:
+        cout << "How many miles did you ride on Friday?: ";
+        cin >> userInput;
+          if(userInput < 0) {
+            cout << "Miles must be 0 or greater!" << endl;
+            cout << "How many miles did you ride on Friday?: ";
+            cin >> userInput;
+          }
+        totalMiles += userInput;
+      case 5:
+        cout << "How many miles did you ride on Saturday?: ";
+        cin >> userInput;
+          if(userInput < 0) {
+            cout << "Miles must be 0 or greater!" << endl;
+            cout << "How many miles did you ride on Saturday?: ";
+            cin >> userInput;
+          }
+        totalMiles += userInput;
+      case 6:
+        cout << "How many miles did you ride on Sunday?: ";
+        cin >> userInput;
+          if(userInput < 0) {
+            cout << "Miles must be 0 or greater!" << endl;
+            cout << "How many miles did you ride on Sunday?: ";
+            cin >> userInput;
+          }
+        totalMiles += userInput;
+    } 
+  }
+
   return 0;
 }
 
 void welcome() {
-  cout << "TODO: Implement welcome function" << endl;
+  cout << "Welcome to my Miles Tracker Program!" << endl;
 }
 
 void goodbye() {
-  cout << GOOBYE_MESSAGE << endl;
-  cout << "TODO: Implement goodbye function" << endl;
+  cout << "\nThank you for using my program!" << endl;
 }
-
-
-// ------------- DESIGN -------------
-/* 
-Program Name:
-
-Program Description:
-
-Design:
-A. INPUT
-Define the input variables including name data type. 
-
-B. OUTPUT
-Define the output variables including data types. 
-
-C. CALCULATIONS
-Describe calculations used by algorithms in step D.  
-List all formulas. 
-If there are no calculations needed, state there are no calculations.
-
-D. LOGIC and ALGORITHMS
-Design the logic of your program using pseudocode or flowcharts. 
-Use conditionals, loops, functions or array constructs.
-List the steps in transforming inputs into outputs. 
-https://github.com/Glen-Sasek-PCC-Instructor/2025-06-22/blob/main/Pseudocode-Reference.txt
-
-
-SAMPLE RUNS
-~~~~~~~~~~ TEST 1 ~~~~~~~~~~
-Welcome to my Miles Tracker program.
-How many miles do you want to ride this week? 50
-How many miles did you ride on Sunday? -9
-Miles must be 0 or greater!
-How many miles did you ride on Sunday? -3
-Miles must be 0 or greater!
-How many miles did you ride on Sunday? 0
-How many miles did you ride on Monday? 10
-How many miles did you ride on Tuesday? 10
-How many miles did you ride on Wednesday? 10
-How many miles did you ride on Thursday? 10
-How many miles did you ride on Friday? 10
-How many miles did you ride on Saturday? 0
-You rode 50 miles this week.
-Good job! You met your goal!
-Keep riding!
-
-~~~~~~~~~~ TEST 2 ~~~~~~~~~~
-Welcome to my Miles Tracker program.
-How many miles do you want to ride this week? 100
-How many miles did you ride on Sunday? 10
-How many miles did you ride on Monday? 10
-How many miles did you ride on Tuesday? 10
-How many miles did you ride on Wednesday? 10
-How many miles did you ride on Thursday? 10
-How many miles did you ride on Friday? 10
-How many miles did you ride on Saturday? 10
-You rode 70 miles this week.
-Uh oh! You missed your goal by 30 miles!
-Keep riding!
-
-~~~~~~~~~~ TEST 3 ~~~~~~~~~~
-Welcome to my Miles Tracker program.
-How many miles do you want to ride this week? 50
-How many miles did you ride on Sunday? 10
-How many miles did you ride on Monday? 10
-How many miles did you ride on Tuesday? 10
-How many miles did you ride on Wednesday? 10
-How many miles did you ride on Thursday? 10
-How many miles did you ride on Friday? 10
-How many miles did you ride on Saturday? 10
-You rode 70 miles this week.
-Great job! You exceeded your goal by 20 miles!
-Keep riding!
-
-~~~~~~~~~~ TEST 4 ~~~~~~~~~~
-Welcome to my Miles Tracker program.
-How many miles do you want to ride this week? 0
-No miles were tracked this week.
-Keep riding!
-
-~~~~~~~~~~ TEST 5 ~~~~~~~~~~
-Welcome to my Miles Tracker program.
-How many miles do you want to ride this week? -10
-No miles were tracked this week.
-Keep riding!
-
-*/
