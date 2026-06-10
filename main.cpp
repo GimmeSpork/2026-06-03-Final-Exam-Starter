36e32447ae74c9df52b16f1f547e35fff523d14c
GimmeSpork <The.horse.butch@gmail.com>
2026-06-10 06:09:54 +0000

    Finished?

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@
 # Sources:          zybooks, assignment resources, given starter template.
 #******************************************************************************/
 #include <iostream>
-
+#include <limits>
 using namespace std;
 
 // Function prototypes   
@@ -61,93 +61,61 @@ int main() {
 void getInput(int &goal) {
   cout << "\nHow many miles do you want to ride this week?: ";
   cin >> goal;
+  cout << endl;
 }
 
 // Function to calculate total miles
 int calcTotal() {
 
   // Decalare variables
+  string userPrompt;
+
   int totalMiles = 0;
   int userInput = 0;
   int goal;
+
 
   // For loop for days of the week
   for(int i = 0; i <= 6; i++){
     switch(i) { // Cases for each day
       case 0:
-        cout << "\nHow many miles did you ride on Sunday?: ";
-        cin >> userInput;
-          while(userInput < 0) { // While userInput is greater than 0
-            cout << "Miles must be 0 or greater!" << endl;
-            cout << "How many miles did you ride on Sunday?: ";
-            cin >> userInput;
-          }
-        totalMiles += userInput; // Update totalMiles
-      break;
+        userPrompt = "How many miles did you ride on Sunday?: ";
+        break;
       case 1:
-        cout << "How many miles did you ride on Monday?: ";
-        cin >> userInput;
-          while (userInput < 0) { // While userInput is greater than 0
-            cout << "Miles must be 0 or greater!" << endl;
-            cout << "How many miles did you ride on Monday?: ";
-            cin >> userInput;
-          }
-        totalMiles += userInput; // update totalMiles
-      break;
+        userPrompt = "How many miles did you ride on Monday?: ";
+        break;
       case 2:
-        cout << "How many miles did you ride on Tuesday?: ";
-        cin >> userInput;
-          while (userInput < 0) { // While userInput is greater than 0
-            cout << "Miles must be 0 or greater!" << endl;
-            cout << "How many miles did you ride on Tuesday?: ";
-            cin >> userInput;
-          }
-        totalMiles += userInput; // Update totalMiles
-      break;
+        userPrompt = "How many miles did you ride on Tuesday?: ";
+        break;
       case 3:
-        cout << "How many miles did you ride on Wednesday?: ";
-        cin >> userInput;
-          while (userInput < 0) { // While userInput is greater than 0
-            cout << "Miles must be 0 or greater!" << endl;
-            cout << "How many miles did you ride on Wednesday?: ";
-            cin >> userInput;
-          }
-        totalMiles += userInput; // update totalMiles
-      break;
+        userPrompt = "How many miles did you ride on Wednesday?: ";
+        break;
       case 4:
-        cout << "How many miles did you ride on Thursday?: ";
-        cin >> userInput;
-          while (userInput < 0) { // While userInput is greater than 0
-            cout << "Miles must be 0 or greater!" << endl;
-            cout << "How many miles did you ride on Thursday?: ";
-            cin >> userInput;
-          }
-        totalMiles += userInput; // Update totalMiles
-      break;
+        userPrompt = "How many miles did you ride on Thursday?: ";
+        break;
       case 5:
-        cout << "How many miles did you ride on Friday?: ";
-        cin >> userInput;
-          while (userInput < 0) { // While userInput is greater than 0
-            cout << "Miles must be 0 or greater!" << endl;
-            cout << "How many miles did you ride on Friday?: ";
-            cin >> userInput;
-          }
-        totalMiles += userInput; // Update totalMiles
-      break;
+        userPrompt = "How many miles did you ride on Friday?: ";
+        break;
       case 6:
-        cout << "How many miles did you ride on Saturday?: ";
-        cin >> userInput;
-          while (userInput < 0) { // While userInput is greater than 0
-            cout << "Miles must be 0 or greater!" << endl;
-            cout << "How many miles did you ride on Saturday?: ";
-            cin >> userInput;
-          }
-        totalMiles += userInput; // Update totalMiles
-      break;
-    } 
+        userPrompt = "How many miles did you ride on Saturday?: ";
+        break;
+    }
+    cout << userPrompt;
+    cin >> userInput;
+    
+    // Input validation for proper miles
+    while(!cin || userInput < 0) {
+      cout << "Miles must be 0 or greater!" << endl;
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cout << userPrompt;
+      cin >> userInput;
+    }
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    totalMiles += userInput;
   }
 
-  return totalMiles;
+  return totalMiles; // Return the total miles
 }
 
 // Welcome message
